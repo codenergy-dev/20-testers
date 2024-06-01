77765076887e4ea05b0cf1da38783e8bc3c25751
Lucas Lannes <lucaslannesr@gmail.com>
2024-06-01 01:11:46 -0300

    Fix key issue on app listing

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default async function Page({searchParams}: any) {
   return (
     <main>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4">
-        {apps.map(app => <AppCard app={app} />)}
+        {apps.map(app => <AppCard key={app.packageName} app={app} />)}
       </div>
       <div className="join ml-2">
         {Array.from({length: Math.ceil(count/pageSize)}, (_, i) => i).map(i => (
