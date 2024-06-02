1e669cf170dfc20a5c8a7135596b03c4b081b666
Lucas Lannes <lucaslannesr@gmail.com>
2024-06-02 17:52:35 -0300

    Split AppFilter into AppButtonFilter, AppJoinFilter and AppSelectFilter

--- src/components/app-filter/index.tsx
+++ src/components/app-filter/index.tsx
@@ -5,83 +5,137 @@ import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 
 type OrderBy = 'newest' | 'oldest'
+type Type = 'all' | 'app' | 'game'
 
 export function AppFilter() {
   const router = useRouter()
   const [orderBy, setOrderBy] = useState<OrderBy>('newest')
-  const [isGame, setIsGame] = useState<boolean>(false)
+  const [type, setType] = useState<Type>('all')
   const [category, setCategory] = useState<string>('')
 
   useEffect(() => {
     const query = {
       orderBy: 'created',
       sort: orderBy == 'newest' ? 'desc' : 'asc',
-      game: isGame ? 'true' : 'false',
+      type: type,
       category: category,
     }
     router.push('?' + new URLSearchParams(query).toString())
-  }, [orderBy, isGame, category])
+  }, [orderBy, type, category])
+
+  function onTypeChange(type: Type) {
+    setType(type)
+    setCategory('')
+  }
   
   return (
     <div className="px-4 flex flex-row flex-wrap gap-2">
-      <button
-        className={[
-          'btn',
-          orderBy == 'newest' ? 'btn-primary' : 'btn-outline',
-          'btn-sm',
-          'rounded-full',
-        ].join(' ')}
+      <AppButtonFilter
+        label="Newest"
+        selected={orderBy == 'newest'}
         onClick={() => setOrderBy('newest')}
-      >
-        Newest
-      </button>
-      <button
-        className={[
-          'btn',
-          orderBy == 'oldest' ? 'btn-primary' : 'btn-outline',
-          'btn-sm',
-          'rounded-full',
-        ].join(' ')}
+      />
+      <AppButtonFilter
+        label="Oldest"
+        selected={orderBy == 'oldest'}
         onClick={() => setOrderBy('oldest')}
-      >
-        Oldest
-      </button>
-      <button
-        className={[
-          'btn',
-          isGame ? 'btn-primary' : 'btn-outline',
-          'btn-sm',
-          'rounded-full',
-        ].join(' ')}
-        onClick={() => {
-          setIsGame(!isGame)
-          setCategory('')
-        }}
-      >
-        Games
-      </button>
-      {!isGame && (
-        <select
-          className="w-48 select select-bordered select-sm"
-          onChange={(e) => setCategory(e.target.value)}
-        >
-          <option value="">All categories</option>
-          {appCategories.map(category => (
-            <option key={category}>{category}</option>
-          ))}
-        </select>
+      />
+      <div className="join">
+        <AppJoinFilter
+          label="All"
+          selected={type == 'all'}
+          onClick={() => onTypeChange('all')}
+        />
+        <AppJoinFilter
+          label="Apps"
+          selected={type == 'app'}
+          onClick={() => onTypeChange('app')}
+        />
+        <AppJoinFilter
+          label="Games"
+          selected={type == 'game'}
+          onClick={() => onTypeChange('game')}
+        />
+      </div>
+      {type == 'app' && (
+        <AppSelectFilter
+          data={appCategories}
+          placeholder="All categories"
+          onChange={setCategory}
+        />
       )}
-      {isGame && (
-        <select
-          className="w-48 select select-bordered select-sm"
-          onChange={(e) => setCategory(e.target.value)}
-        >
-          <option value="">All categories</option>
-          {gameCategories.map(category => (
-            <option key={category}>{category}</option>
-          ))}
-        </select>
+      {type == 'game' && (
+        <AppSelectFilter
+          data={gameCategories}
+          placeholder="All categories"
+          onChange={setCategory}
+        />
       )}
     </div>
+  )
+}
+
+interface AppButtonFilterProps {
+  label: string
+  selected: boolean
+  onClick: () => void
+}
+
+function AppButtonFilter({label, selected, onClick}: AppButtonFilterProps) {
+  return (
+    <button
+      className={[
+        'btn',
+        selected ? 'btn-primary' : 'btn-outline',
+        'btn-sm',
+        'rounded-full',
+      ].join(' ')}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  )
+}
+
+interface AppJoinFilterProps {
+  label: string
+  selected: boolean
+  onClick: () => void
+}
+
+function AppJoinFilter({label, selected, onClick}: AppJoinFilterProps) {
+  return (
+    <button
+      className={[
+        'btn',
+        selected ? 'btn-primary' : 'btn-outline',
+        'btn-sm',
+        'rounded-full',
+        'join-item',
+      ].join(' ')}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  )
+}
+
+interface AppSelectFilterProps {
+  data: string[]
+  placeholder: string
+  onChange: (value: string) => void
+}
+
+function AppSelectFilter({data, placeholder, onChange}: AppSelectFilterProps) {
+  return (
+    <select
+      className="w-48 select select-bordered select-sm"
+      onChange={(e) => onChange(e.target.value)}
+    >
+      <option value="">{placeholder}</option>
+      {data.map(value => (
+        <option key={value}>{value}</option>
+      ))}
+    </select>
   )
 }
